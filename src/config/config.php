b609134add35e1749a98e0202f4f162c9471fe95
nidrax <nidraxmusic@gmail.com>
2018-01-03 16:29:23 +0100

    Env variable

--- src/config/config.php
+++ src/config/config.php
@@ -10,13 +10,13 @@ return array(
     *       Les signatures de cette environnement sont (bien sûr) légales.
     *
     */
-    'environment' => "demo",
+    'environment' => env('YOUSIGN_ENV', "demo"),
     // Votre identifiant Yousign
-    'login' => "VOTRE_LOGIN",
+    'login' => env('YOUSIGN_LOGIN', "login"),
     // Votre mot de passe Yousign
-    'password' => "VOTRE_MOT_DE_PASSE",
+    'password' => env('YOUSIGN_PASSWORD', "password"),
     // Indique si le mot de passe passé et crypté ou non (A décommenter si le mot de passe passé et crypté)
-    'isEncryptedPassword' => false,
+    'isEncryptedPassword' => env('YOUSIGN_ENCRYPTED', false),
     // clef d'api
-    'api_key' => "VOTRE_API_KEY",
+    'api_key' => env('YOUSIGN_API_KEY', "key"),
 );
